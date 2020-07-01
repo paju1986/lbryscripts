9111dabfef522df4c0940bcf972cca3070dfe2f2
paju1986 <paju1986@gmail.com>
2020-07-01 22:09:03 +0200

    asdfad#

--- script.py
+++ script.py
@@ -5,16 +5,20 @@ import json
 path = os.getcwd()
 with os.scandir(path) as entries:
     for entry in entries:
+        thumbUrl = ""
         fileNameNoExtension = entry.name.split(".")[0]
-        thumbName = fileNameNoExtension + "_thumbnail.png"
-        os.system("ffmpeg -i "+path + "/" + entry.name+" -ss 00:00:01.000 -vframes 1 "+ path + "/" + thumbName)
-        thumbnailParams = {
-            "name": thumbName,
-            "file": path + "/" + thumbName
-        }
-        
-        print(requests.post("https://spee.ch/api/claim/publish",json.dumps(thumbnailParams)).json())
-        
+        if entry.name.split(".")[1] == "mp4":
+            thumbName = fileNameNoExtension + "_thumbnail.png"
+            os.system("ffmpeg -i "+path + "/" + entry.name+" -ss 00:00:01.000 -vframes 1 "+ path + "/" + thumbName)
+            thumbnailParams = {
+                "name": thumbName
+            }
+            files = {'file': open(path + "/" + thumbName,'rb')}
+
+            returnJson = requests.post("https://spee.ch/api/claim/publish", files=files,data=thumbnailParams).json()
+            print(returnJson)
+            thumbUrl = returnJson["data"]["serveUrl"]
+
         params = {
             "method": "publish",
             "params": {
@@ -26,11 +30,12 @@ with os.scandir(path) as entries:
                 "tags": [],
                 "languages": [],
                 "locations": [],
-                "channel_id": "",
+                "channel_id": "e288b001fb5707890b637ced51bfaf8e9ced7837",
+                "thumbnail_url": thumbUrl,
                 "funding_account_ids": [],
                 "preview": False,
                 "blocking": False
                 }
         }
-        print(json.dumps(params))
+        #print(json.dumps(params))
         print(requests.post("http://localhost:5279/",json.dumps(params)).json())
